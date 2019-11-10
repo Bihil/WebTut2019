c4ba3a6d8927346fa817110692e07edd221b8136
Bihil <bihilkon@gmail.com>
2019-11-10 22:53:56 +0100

    created webpack configuration file and finished with html and css files.

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+
+import '../scss/index.scss';
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+import './vendor/fontawesome';
